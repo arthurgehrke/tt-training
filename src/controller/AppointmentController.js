13c555877f7d520a3f2593feb9ab2fc4213ff8b8
ne0xys <leandrolima@me.com>
2020-08-31 01:11:13 -0400

    feat(appoitments): verify time slot with db

--- src/controller/AppointmentController.js
+++ src/controller/AppointmentController.js
@@ -3,35 +3,58 @@ require('dotenv').config()
 const Appointment = require('../models/Appointments')
 const moment = require('moment')
 const jwt = require('jsonwebtoken')
-const { promisify } = require('util') 
+const { promisify } = require('util') // ???
 
 class AppointmentController {
-    async getAppointments(req,res) {
-        const appointments = await Appointment.find()
+  async getAppointments(_, res) {
+    const appointments = await Appointment.find()
 
-        return res.status(200).json(appointments)
+    return res.status(200).json(appointments)
+  }
+
+  async createAppointment(req, res) {
+    const { start_time, end_time } = req.body
+    const [, token] = req.headers.authorization.split(' ')
+
+    function consoleOutput(start, end) {
+      start = moment(start).format('llll')
+      end = moment(end).format('llll')
+
+      console.log('New Appointment \nStart: ' + start + '\nEnd: ' + end)
     }
+    consoleOutput(start_time, end_time)
 
+    try {
+      if (moment(end_time).isSameOrBefore(moment(start_time)))
+        throw new Error(' Invalid input ')
 
-    async createAppointment(req,res){
-        const { start_time, end_time } = req.body
-        const [, token] = req.headers.authorization.split(' ')
+      const verifyAvailability = await Appointment.findOne({
+        $or: [
+          { start_time: { $gte: start_time, $lt: end_time } },
+          { start_time: { $lt: start_time }, end_time: { $gt: start_time } }
+        ]
+      })
 
-        try{ 
-            const verifyAvailability = await Appointment.findOne({ start_time })
+      if (verifyAvailability)
+        throw new Error('This appointment is already taken')
 
-            if(verifyAvailability) throw new Error('This appointment is already taken')
-            const formatedDate = moment().hour(start_time)
+      const decodedToken = await promisify(jwt.verify)(
+        token,
+        process.env.TOKEN_SECRET
+      )
 
-            const decodedToken = await promisify(jwt.verify)(token, process.env.TOKEN_SECRET)
-            
-            const appointment = await Appointment.create({ user_id: decodedToken.userId, start_time, end_time})
-            
-            return res.status(201).json({appointment})
-        } catch(err){
-            return res.status(400).json({err: err.message})
-        }
-    } 
+      const appointment = await Appointment.create({
+        user_id: decodedToken.userId,
+        start_time,
+        end_time
+      })
+      console.log('----Appoitment Created----')
+      return res.status(201).json({ appointment })
+    } catch (err) {
+      console.log('----Appoitment already taken----')
+      return res.status(400).json({ err: err.message })
+    }
+  }
 }
 
 module.exports = new AppointmentController()
--- src/models/Appointments.js
+++ src/models/Appointments.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose')
 const { MongoError } = require('mongodb')
 
 const AppointmentSchema = new mongoose.Schema({
-    user_id: { 
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    start_time: { type: String},
-    end_time: { type: String},
-    status: { type: String, enum: ['Pending', 'Booked','Canceled'], default: 'Pending'}, 
-    createdAt: { type: Date, default: Date.now},
-  })
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  start_time: { type: Date },
+  end_time: { type: Date },
+  status: {
+    type: String,
+    enum: ['Pending', 'Booked', 'Canceled'],
+    default: 'Pending'
+  },
+  createdAt: { type: Date, default: Date.now }
+})
 
 module.exports = mongoose.model('Appointment', AppointmentSchema)
